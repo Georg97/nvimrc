90309ccf333f598a9067d92854c2c2760b40edc1
georg <georghirsch97@gmail.com>
2023-07-03 00:03:52 +0200

    got a lazyvim config with custom snippets prepared

---
+++ lua/plugins/_theme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add rose-pine
+  { "rose-pine/neovim" },
+
+  -- Configure LazyVim to load rose-pine
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  -- { "rose-pine/neovim" },
+  -- { "folke/trouble.nvim", enabled = false },
+  -- { "L3MON4D3/LuaSnip", enabled = false },
+}
