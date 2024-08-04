516c34a2c13491465861a5ac17bdeedabaf2de79
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-04 14:59:11 +0200

    commented out code from native-lsp.lua

--- after/native-lsp.lua
+++ after/native-lsp.lua
@@ -1,5 +1,5 @@
-vim.lsp.start_client({
-    name = 'my-server-name',
-    cmd = { 'lua-language-server' },
-    root_dir = vim.fs.dirname(vim.fs.find({'setup.py'}, {upward = true})[1])
-})
+-- vim.lsp.start_client({
+--     name = 'my-server-name',
+--     cmd = { 'lua-language-server' },
+--     root_dir = vim.fs.dirname(vim.fs.find({'setup.py'}, {upward = true})[1])
+-- })
